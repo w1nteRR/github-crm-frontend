b4b2a995cb1551f4aa0bf47326496b44480d62d6
w1nteRR <mykola796@gmail.com>
2025-04-30 15:16:49 +0300

    add layouts

---
+++ src/navigation/layouts/auth.layout.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import { Outlet } from 'react-router';
+
+export const AuthLayout: FC = () => {
+  return (
+    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className='w-full max-w-sm '>
+        <Outlet />
+      </div>
+    </main>
+  );
+};
+
---
+++ src/navigation/layouts/main.layout.tsx
@@ -0,0 +1,3 @@
+export const MainLayout = () => {
+  return <><p>main layout</p></>;
+};
