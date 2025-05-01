47a21af95b128ed2cee2a158ab39cfa8fe9eaf09
w1nteRR <mykola796@gmail.com>
2025-05-01 19:19:22 +0300

    init main page & layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { router } from '@/navigation/routes.tsx';
+import { AuthProvider } from '@/providers/auth.provider.tsx';
+import { DialogProvider } from '@/providers/dialog.provider.tsx';
 
 import './index.css';
-import { router } from '@/navigation/routes.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <DialogProvider>
+          <RouterProvider router={router} />
+        </DialogProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/navigation/layouts/main.layout.tsx
+++ src/navigation/layouts/main.layout.tsx
@@ -1,3 +1,9 @@
+import { Outlet } from 'react-router';
+
 export const MainLayout = () => {
-  return <><p>main layout</p></>;
+  return <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className='w-full'>
+      <Outlet />
+    </div>
+  </main>;
 };
